de938483ac3299685e798139a622a07d34778022
Kumar Priyansh <bobbyskhs@gmail.com>
2020-08-31 00:57:08 +0530

    update results

--- app/scripts/search.js
+++ app/scripts/search.js
@@ -1,4 +1,5 @@
 const searchBox = document.getElementById('searchBox');
+const results = document.getElementById('results');
 const natural = require('natural');
 const wordnet = new natural.WordNet();
 
@@ -7,6 +8,12 @@ searchBox.addEventListener('keypress', (e) => {
         const word = searchBox.value;
         wordnet.lookup(word, details => {
             console.log(details);
+            details.forEach(detail => {
+                const definition = document.createElement('p');
+                definition.innerHTML = detail.def;
+                definition.className = 'definition';
+                results.appendChild(definition);
+            });
         });
     }
 });
