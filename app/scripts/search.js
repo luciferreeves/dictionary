268ea465afcb7c2244bd1dc004cf458e0669ffdc
Kumar Priyansh <bobbyskhs@gmail.com>
2020-08-31 00:43:50 +0530

    Enlarged search text and search event fired on enter press

--- app/scripts/search.js
+++ app/scripts/search.js
@@ -2,9 +2,11 @@ const searchBox = document.getElementById('searchBox');
 const natural = require('natural');
 const wordnet = new natural.WordNet();
 
-searchBox.addEventListener('keyup', () => {
-    const word = searchBox.value;
-    wordnet.lookup(word, details => {
-        console.log(details);
-    });
+searchBox.addEventListener('keypress', (e) => {
+    if (e.keyCode === 13) {
+        const word = searchBox.value;
+        wordnet.lookup(word, details => {
+            console.log(details);
+        });
+    }
 });
